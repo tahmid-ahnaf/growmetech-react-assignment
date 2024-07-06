8582bee7a3fe329e4bc4623ef139a43c721d5a0b
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-07-06 18:07:36 +0600

    navbar added

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,5 +1,6 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const lightTheme = createTheme({
   palette: {
@@ -10,6 +11,7 @@ const Main = () => {
   return (
     <div>
       <ThemeProvider theme={lightTheme}>
+        <Navbar></Navbar>
         <Outlet></Outlet>
       </ThemeProvider>
     </div>
